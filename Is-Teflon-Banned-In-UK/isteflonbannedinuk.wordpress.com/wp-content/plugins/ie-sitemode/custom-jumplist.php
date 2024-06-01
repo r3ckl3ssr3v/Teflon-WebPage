a4629b801c622536a1e600e859743d5b9226e7ee
r3ckl3ssr3v <sabyasachipal335@gmail.com>
2024-06-01 18:02:37 +0530

    Website for a client for his article against Teflon

---
+++ Is-Teflon-Banned-In-UK/isteflonbannedinuk.wordpress.com/wp-content/plugins/ie-sitemode/custom-jumplist.php
@@ -0,0 +1,2 @@
+function wpcom_custom_jumplist_reader(){window.external.msSiteModeCreateJumplist("Recent posts");window.external.msSiteModeAddJumpListItem("Hello World!","https://isteflonbannedinuk.wordpress.com/2023/03/06/hello-world/","https://s1.wp.com/i/favicon.ico");}
+try{if(window.external && 'msIsSiteMode' in window.external){if (window.external.msIsSiteMode()){wpcom_custom_jumplist_reader();wpcom_sitemode_comment_notify();}}}catch(e){}
